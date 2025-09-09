9713795427f44ff4634a64cb027ba054f3e688a0
DarkQG <sw23d001@mandakh.edu.mn>
2025-09-09 16:20:41 +0800

    Create page.tsx

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div style={{ padding: '2rem', maxWidth: '600px', margin: '0 auto' }}>
+      <h1>Бидний тухай</h1>
+      <p>
+        Манай вэбсайтад тавтай морилно уу!
+      </p>
+    </div>
+  );
+};
+
+export default About;
